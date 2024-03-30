4e77ee1fb9a07786023ae5ad3a69247bdcb0ddf0
Jun Wei <53926665+0xjunwei@users.noreply.github.com>
2024-03-30 10:09:09 +0900

    Flowtest
    Added more flowtest to test functions within the contract, changed soulbound solidity to 0.8.23 instead of using ^ as it is not a library

--- fhenix hardhat code/test/unit/01-flowtest.js
+++ fhenix hardhat code/test/unit/01-flowtest.js
@@ -7,7 +7,7 @@ describe("SoulBound", async function () {
   let admin;
   let client;
   const client_fhenix = new FhenixClient({ provider: ethers.provider });
-  beforeEach(async function () {
+  before(async function () {
     // deploy Proofers
     // using hardhat-deploy
     // get accounts from ethers
@@ -28,23 +28,130 @@ describe("SoulBound", async function () {
   // checks tries to mint again should not work
   describe("mint a second time", function () {
     it("Fails if user possess a token already", async function () {
-      // Await the completion of the first mint operation.
-      const receipt = await soulContract.mint(client.getAddress(), 0);
-      await receipt.wait();
-
-      await expect(soulContract.mint(client.getAddress(), 0)).to.be.reverted;
+      await expect(soulContract.mint(client.getAddress(), 1)).to.be.reverted;
     });
   });
 
   // check Date Of Birth adding
   // checks if adding of DOB is accurate
   describe("adding Individual DOB", async function () {
-    it("Should be succeed at adding DOB as owner of contract", async function () {
+    it("Should succeed at adding DOB as owner of contract", async function () {
       const eDOB = await client_fhenix.encrypt(
         638656385,
         EncryptionTypes.uint32
       );
       await expect(soulContract.addUserDOBDetails(client.getAddress(), eDOB));
+    });
+  });
+
+  // should fail if adding a second time
+  // Hardhat is not registering that it reverted which is weird
+  // FHE lib is not doing a normal reversion
+  /*describe("Negative Flow DOB", async function () {
+    it("Should fail at adding DOB a 2nd time", async function () {
+      const eDOB = await client_fhenix.encrypt(
+        638656385,
+        EncryptionTypes.uint32
+      );
+      await expect(soulContract.addUserDOBDetails(client.getAddress(), eDOB)).to
+        .be.reverted;
+    });
+  });*/
+
+  // Adds admin to contract
+  describe("adding Admin", async function () {
+    it("Should succeed at adding a admin as a deployer", async function () {
+      await expect(soulContract.addAdmin(admin.getAddress()));
+    });
+  });
+  // tries to add self into admin but not as deployer
+  describe("Negative Flow Admin", async function () {
+    it("Should fail if i am not a deployer and trying to get admin", async function () {
+      await expect(soulContract.connect(client).addAdmin(client.getAddress()))
+        .to.be.reverted;
+    });
+  });
+
+  // As admin tries to Add medical Data & lastExamineDate;
+  describe("Adding Medical data", async function () {
+    it("Should succeed as Admin", async function () {
+      const eMedicalData = await client_fhenix.encrypt(
+        2,
+        EncryptionTypes.uint8
+      );
+      const eLastExamineDate = await client_fhenix.encrypt(
+        638656385,
+        EncryptionTypes.uint32
+      );
+      await expect(
+        soulContract
+          .connect(admin)
+          .addMedicalData(client.getAddress(), eMedicalData, eLastExamineDate)
+      );
+    });
+  });
+  // Add criminalRecord;
+  describe("Adding Criminal Records", async function () {
+    it("Should succeed as Admin", async function () {
+      const eCriminalRecord = await client_fhenix.encrypt(
+        2,
+        EncryptionTypes.uint8
+      );
+      await expect(
+        soulContract
+          .connect(admin)
+          .addCriminalRecord(client.getAddress(), eCriminalRecord)
+      );
+    });
+  });
+  // Add fertilityMeasure;
+  describe("Adding Fertility Measure", async function () {
+    it("Should succeed as Admin", async function () {
+      const eFertility = await client_fhenix.encrypt(2, EncryptionTypes.uint8);
+      await expect(
+        soulContract
+          .connect(admin)
+          .addFertilityCount(client.getAddress(), eFertility)
+      );
+    });
+  });
+  // Add marriageStatus;
+  describe("Adding Marriage Status", async function () {
+    it("Should succeed as Admin", async function () {
+      const eMarriageStatus = await client_fhenix.encrypt(
+        2,
+        EncryptionTypes.uint8
+      );
+      await expect(
+        soulContract
+          .connect(admin)
+          .addMarriageStatus(client.getAddress(), eMarriageStatus)
+      );
+    });
+  });
+  // Add rating;
+  describe("Adding Rating", async function () {
+    it("Should succeed as Admin", async function () {
+      const eRating = await client_fhenix.encrypt(2, EncryptionTypes.uint8);
+      await expect(
+        soulContract.connect(admin).addRating(client.getAddress(), eRating)
+      );
+    });
+  });
+
+  // Trying to read the data out and validating
+  // Inserted 2 above, should get back 2
+  describe("Reading Medical Data", async function () {
+    it("Should succeed as Admin", async function () {
+      const permit = await getPermit(tester.address, ethers.provider);
+      client.storePermit(permit);
+      const permission = client.extractPermitPermission(permit);
+      response0 = await contractInstance.retrieveMedicalData(
+        clientAcc.address,
+        permission
+      );
+      const plaintext = client.unseal(tester.address, response0);
+      assert.equal(response, 2, "The result should match 2 was inserted");
     });
   });
 });
